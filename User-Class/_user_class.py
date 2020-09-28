c89c9815f22bb65cee8103471a0111608112e3a9
Lambels <69460943+Lambels@users.noreply.github.com>
2020-09-28 13:16:08 +0300

    Update _user_class.py

--- User-Class/_user_class.py
+++ User-Class/_user_class.py
@@ -7,6 +7,7 @@ class User:
     def __init__(self, name):
         self.name = name
         self.messages = []
+        self.time = []
 
 
     def check(self, message):
@@ -14,6 +15,22 @@ class User:
         for i in self.messages:
             if self.messages.count(i) >= 4:
                 return i in str(message.content.lower())
+
+
+    def set_time(self, time):
+        now = str(time)
+        self.time.append((list(now)[17], list(now)[18]))
+
+
+    def check_timer(self) -> bool:
+        time_1 = int(self.time[0][0]) * 60 + int(self.time[0][1])
+        time_2 = int(self.time[1][0]) * 60 + int(self.time[1][1])
+        ans = time_2 - time_1
+        if ans <= 3:
+            return True
+        else:
+            return False
+
 
     @classmethod
     def check_if_typed(cls, user):
